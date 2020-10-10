cb5e3aa07fad849673e0bf1d1adcb61b092410d7
Paul <paul.jeffress@gmail.com>
2020-10-10 16:51:16 +1100

    Ready to start work post split of source into multiple files and compile working still.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
  * 
  * I'm running this on one of my Heltec ESP32+LORA boards, with built in OLED.
  * 
- * 8/10/20 - So far I have a working irradiance monitor that displays current irradiance to its onboard OLED effectively.
- *           Next step is to make it log it over a network connection to a host.
- *           I’ll freeze “PJ_ESP32_Heltec_solar_baseline_monitor.ino” where it is and leave that as a working sketch for 
- *           the non networked monitor and copy that today 
- *           to “PJ_ESP32_Heltec_solar_baseline_monitor and logger.ino” and do all additional features in that code.
- * 9/10/20 - Migrated project to platformio and broke it into a bunch of sub files
+ * 08/10/20 - So far I have a working irradiance monitor that displays current irradiance to its onboard OLED effectively.
+ *            Next step is to make it log it over a network connection to a host.
+ *            I’ll freeze “PJ_ESP32_Heltec_solar_baseline_monitor.ino” where it is and leave that as a working sketch for 
+ *            the non networked monitor and copy that today 
+ *            to “PJ_ESP32_Heltec_solar_baseline_monitor and logger.ino” and do all additional features in that code.
+ * 09/10/20 - Migrated project to platformio and broke it into a bunch of sub files
+ * 10/10/20 - Migrated source files to GitHub managed.
  * 
  */
 
